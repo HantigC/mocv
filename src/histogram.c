3b255ec9427576401dccbf21be02a1dda87d4bec
HantigC <cristianhantig96@gmail.com>
2023-10-09 19:48:53 +0300

    change bucket from array bucket struct to array of int

--- src/histogram.c
+++ src/histogram.c
@@ -4,12 +4,12 @@
 #include "stdio.h"
 #include <stdlib.h>
 
-histogram *make_empty_hist() {
+histogram *alloc_empty_hist() {
     histogram *hist = malloc(sizeof(histogram));
     return hist;
 }
 
-bucket *make_bucket(float start, float end, float count) {
+bucket *alloc_bucket(float start, float end, float count) {
     bucket *buck = (bucket *)malloc(sizeof(bucket));
     buck->start = start;
     buck->end = end;
@@ -17,108 +17,117 @@ bucket *make_bucket(float start, float end, float count) {
     return buck;
 }
 
-float histogram_total(histogram *hist) {
+float histogram_total(histogram hist) {
     float total = 0.0f;
-    for (int bin = 0; bin < hist->bins; bin++) {
-        total += hist->buckets[bin].count;
+    for (int bin = 0; bin < hist.bins; bin++) {
+        total += hist.counts[bin];
     }
     return total;
 }
 
-histogram *make_hist(int bins, float min_value, float max_value) {
-    histogram *hist = make_empty_hist();
-    init_hist(hist, bins, min_value, max_value);
+histogram make_hist(int bins, float min_value, float max_value, float eps) {
+    histogram hist = {0};
+    init_hist(&hist, bins, min_value, max_value, eps);
     return hist;
 }
 
-void init_hist(histogram *hist, int bins, float min_value, float max_value) {
+void init_hist(histogram *hist, int bins, float min_value, float max_value,
+               float eps) {
     hist->bins = bins;
-    hist->buckets = (bucket *)calloc(bins, sizeof(bucket));
-    hist->step = (max_value - min_value + 0.0000001f) / bins;
-    for (int bin = 0; bin < hist->bins; bin++) {
-        hist->buckets[bin].start = bin * hist->step;
-        hist->buckets[bin].count = 0;
-        hist->buckets[bin].end = bin * hist->step + hist->step;
-    }
+    hist->counts = (int *)calloc(bins, sizeof(int));
+    hist->step = (max_value - min_value + eps) / bins;
+    hist->eps = eps;
     hist->min_value = min_value;
     hist->max_value = max_value;
-    hist->total = 0;
+    hist->total_count = 0;
 }
-
-bucket *copy_bucket(bucket *src) { return make_bucket(src->start, src->end, src->count); }
 
 void add_to_hist(histogram *hist, float x) {
     int bin = (x - hist->min_value) / hist->step;
     bin = CLAMP(bin, 0, hist->bins - 1);
-    hist->buckets[bin].count += 1.0f;
-    hist->total += 1.0f;
+    hist->counts[bin] += 1;
+    hist->total_count += 1;
 }
 
-histogram *copy_hist(histogram *hist) {
-    histogram *dest = make_hist(hist->bins, hist->min_value, hist->max_value);
-    for (int i = 0; i < hist->bins; i++) {
-        dest->buckets[i] = *copy_bucket(&(hist->buckets[i]));
+histogram copy_hist(histogram hist) {
+    histogram dest = hist;
+    hist.counts = (int *)calloc(hist.bins, sizeof(int));
+    for (int i = 0; i < hist.bins; i++) {
+        dest.counts[i] = hist.counts[i];
     }
-    dest->step = hist->step;
-    dest->min_value = hist->min_value;
-    dest->max_value = hist->max_value;
     return dest;
 }
 
-void normalize_hist_(histogram *hist) {
-    float total;
-    total = hist->buckets[0].count;
-    for (int i = 1; i < hist->bins; i++) {
-        total += hist->buckets[1].count;
-    }
-    for (int i = 0; i < hist->bins; i++) {
-        hist->buckets[i].count /= total;
+void normalize_hist_(histogram hist) {
+    for (int i = 0; i < hist.bins; i++) {
+        hist.counts[i] /= hist.total_count;
     }
 }
-histogram *normalize_hist(histogram *hist);
-image *render_histogram(histogram *hist, int height, int width, color *color) {
-    image *img = make_image(height, width, 3);
-    return render_histogram_(img, hist, color);
+histogram normalize_hist(histogram hist) {
+    histogram dest = copy_hist(hist);
+    normalize_hist_(dest);
+    return dest;
+}
+image render_histogram(histogram hist, int height, int width, rgb color) {
+    image img = make_image(height, width, 3);
+    render_histogram_(img, hist, color);
+    return img;
 }
 
-float histogram_max_count(histogram *hist) {
-    float max_count = hist->buckets[0].count;
-    for (int bin = 1; bin < hist->bins; bin++) {
-        max_count = MAX(max_count, hist->buckets[bin].count);
+int histogram_max_count(histogram hist) {
+    int max_count = hist.counts[0];
+    for (int bin = 1; bin < hist.bins; bin++) {
+        max_count = MAX(max_count, hist.counts[bin]);
     }
     return max_count;
 }
 
-image *render_histogram_(image *img, histogram *hist, color *color) {
-    float offset = 10.0f;
-    float bin_width = (float)(img->width - 2.0f * offset) / hist->bins;
+void render_histogram_(image img, histogram hist, rgb color) {
+    int offset = img.width * 0.01f;
+    float bin_width = (float)(img.width - 2.0f * offset) / (float)hist.bins;
+
     int bin_height;
-    float max_count = histogram_max_count(hist);
-    point2di *tl = (point2di *)malloc(sizeof(point2di));
-    point2di *br = (point2di *)malloc(sizeof(point2di));
+    point2di tl;
+    point2di br;
 
-    for (int bin = 0; bin < hist->bins; bin++) {
-        tl->x = bin * bin_width + offset;
-        bin_height = (img->height - offset) * hist->buckets[bin].count / max_count;
-        tl->y = img->height - bin_height;
-        br->x = bin * bin_width + bin_width + offset;
-        br->y = img->height;
-        fill_rectangle_tlbr_(img, tl, br, color);
+    float max_count = (float)histogram_max_count(hist);
+    for (int bin = 0; bin < hist.bins; bin++) {
+        tl.x = bin * bin_width + offset;
+        bin_height = (img.height - offset) * hist.counts[bin] / max_count;
+        tl.y = img.height - bin_height;
+        br.x = bin * bin_width + bin_width + offset;
+        br.y = img.height;
+        fill_rectangle_tlbr_rgb_(img, tl, br, color);
     }
-    free(tl);
-    free(br);
-    return img;
 }
 
-void print_bucket(bucket *buck) {
-    printf("Bucket(start=%f, end=%f, count=%f)", buck->start, buck->end, buck->count);
+void print_bucket(bucket buck) {
+    printf("Bucket(start=%f, end=%f, count=%f)", buck.start, buck.end,
+           buck.count);
 }
-void print_hist(histogram *hist) {
-    printf("Histogram(bins=%d, step=%f, min_value=%f, max_value=%f, total=%f", hist->bins,
-           hist->step, hist->min_value, hist->max_value, hist->total);
-    for (int bin = 0; bin < hist->bins; bin++) {
-        printf("bin[%d]:", bin);
-        print_bucket(&hist->buckets[bin]);
+void print_hist(histogram hist) {
+    printf("Histogram(bins=%d, step=%f, min_value=%f, max_value=%f, total=%d",
+           hist.bins, hist.step, hist.min_value, hist.max_value,
+           hist.total_count);
+    for (int bin = 0; bin < hist.bins; bin++) {
+        printf("bin[%d]=%d:", bin, hist.counts[bin]);
         printf("\n");
+    }
+}
+
+void render_histogram_at_(image img, histogram hist, int start_bin, int end_bin,
+                          int max_count, float bin_width, int offset,
+                          rgb color) {
+    point2di tl;
+    point2di br;
+    int bin_height;
+    for (int bin = start_bin; bin < end_bin; bin++) {
+        tl.x = bin * bin_width + offset;
+        bin_height =
+            (img.height - offset) * hist.counts[bin] / (float)max_count;
+        tl.y = img.height - bin_height;
+        br.x = bin * bin_width + bin_width + offset;
+        br.y = img.height;
+        fill_rectangle_tlbr_rgb_(img, tl, br, color);
     }
 }
--- src/histogram.h
+++ src/histogram.h
@@ -9,27 +9,33 @@ typedef struct bucket {
 } bucket;
 
 typedef struct histogram {
-    bucket *buckets;
+    int *counts;
     int bins;
     float step;
     float min_value;
     float max_value;
-    float total;
+    int total_count;
+    float eps;
 } histogram;
 
-bucket *copy_bucket(bucket *);
-histogram *make_hist(int bins, float min_value, float max_value);
-histogram *make_empty_hist();
-histogram *copy_hist(histogram *);
-void init_hist(histogram *, int bins, float min_value, float max_value);
-bucket *make_bucket(float start, float end, float count);
+histogram make_hist(int bins, float min_value, float max_value, float eps);
+histogram *alloc_empty_hist();
+histogram copy_hist(histogram);
+void init_hist(histogram *, int bins, float min_value, float max_value,
+               float eps);
+bucket *alloc_bucket(float start, float end, float count);
 
-void normalize_hist_(histogram *hist);
-histogram *normalize_hist(histogram *hist);
-float histogram_total(histogram *hist);
+void normalize_hist_(histogram hist);
+histogram normalize_hist(histogram hist);
+float histogram_total(histogram hist);
 
-image *render_histogram(histogram *, int height, int width, color *color);
-image *render_histogram_(image *img, histogram *hist, color *color);
-void print_hist(histogram *hist);
+image render_histogram(histogram, int height, int width, rgb color);
+void render_histogram_(image img, histogram hist, rgb color);
+void print_hist(histogram hist);
 void add_to_hist(histogram *hist, float x);
+
+int histogram_max_count(histogram hist);
+void render_histogram_at_(image img, histogram hist, int start_bin, int end_bin,
+                          int max_count, float bin_width, int offset,
+                          rgb color);
 #endif
