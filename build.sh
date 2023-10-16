430dc335474eae3d5d2422d5b4c223043a81a06b
HantigC <cristianhantig96@gmail.com>
2023-10-16 14:14:15 +0300

    fix parameter check for build.sh

--- build.sh
+++ build.sh
@@ -3,5 +3,7 @@ if [ ! -z "$1" ]; then
     BUILD_TYPE=${1}
     shift
 fi
-cmake -S. -B build/$BUILD_TYPE -DCMAKE_BUILD_TYPE=$BUILD_TYPE
-cmake --build build/$BUILD_TYPE --config $BUILD_TYPE
+echo "Start build for ${BUILD_TYPE}"
+cmake -S. -B build/${BUILD_TYPE} -DCMAKE_BUILD_TYPE=${BUILD_TYP}
+cmake --build build/${BUILD_TYPE} --config ${BUILD_TYPE}
+echo "End build for ${BUILD_TYPE}"
